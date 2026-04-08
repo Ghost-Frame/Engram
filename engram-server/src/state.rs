fefdf6b53f17bec66c5732ef8f292e55b607f26c
Ghost-Frame <user@example.com>
2026-04-07 23:48:39 -0400

    Implement episode and personality sync APIs

--- engram-server/src/state.rs
+++ engram-server/src/state.rs
@@ -3,6 +3,7 @@ use kleos_lib::config::Config;
 use kleos_lib::db::Database;
 use kleos_lib::embeddings::EmbeddingProvider;
 use kleos_lib::reranker::Reranker;
+use kleos_lib::services::brain::BrainManager;
 
 #[derive(Clone)]
 pub struct AppState {
@@ -10,4 +11,5 @@ pub struct AppState {
     pub config: Arc<Config>,
     pub embedder: Option<Arc<dyn EmbeddingProvider>>,
     pub reranker: Option<Arc<Reranker>>,
+    pub brain: Option<Arc<BrainManager>>,
 }
